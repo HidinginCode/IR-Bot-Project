bc569e4ebcebc6ad013f73d5f7c9a077aba07168
HidinginCode <86361594+HidinginCode@users.noreply.github.com>
2022-01-07 16:10:18 +0100

    Improved GUI

--- src/main/java/gui.java
+++ src/main/java/gui.java
@@ -2,20 +2,23 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 public class gui extends JFrame {
-    // Anfang Attribute
     private JTextField jTextField1 = new JTextField();
     private JButton bSend = new JButton();
-    private JTextArea jTextArea1 = new JTextArea("");
-    private JScrollPane jTextArea1ScrollPane = new JScrollPane(jTextArea1);
-    // Ende Attribute
+    private JTextPane chatWindow = new JTextPane();
+    private JScrollPane jTextArea1ScrollPane = new JScrollPane(chatWindow);
+
 
     public gui() {
-        // Frame-Initialisierung
+        //Create Frame
         super();
-        jTextArea1.setEditable(false);  //TextArea1 can not be edited by the user directly
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        chatWindow.setEditable(false);
         int frameWidth = 799;
         int frameHeight = 543;
         setSize(frameWidth, frameHeight);
@@ -27,8 +30,8 @@ public class gui extends JFrame {
         setResizable(false);
         Container cp = getContentPane();
         cp.setLayout(null);
-        // Anfang Komponenten
 
+        //Create Components
         jTextField1.setBounds(48, 432, 481, 33);
         jTextField1.setToolTipText("Put your message here");
         cp.add(jTextField1);
@@ -37,14 +40,37 @@ public class gui extends JFrame {
         bSend.setMargin(new Insets(2, 2, 2, 2));
         bSend.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
-                jTextField1.setText(""); //Should reset text in JTextField
+                button_pressed(evt);
             }
         });
         cp.add(bSend);
         jTextArea1ScrollPane.setBounds(48, 32, 481, 345);
         cp.add(jTextArea1ScrollPane);
-        // Ende Komponenten
-
         setVisible(true);
+        //Everything is initialized --> now put Welcome Message
+        coloredText(chatWindow, "Botti: Hey, my name is Botti.\n", Color.RED);
+        coloredText(chatWindow, "Botti: How can I help you?\n", Color.RED);
     }
+
+    //Button Actions
+    public void button_pressed(ActionEvent evt) {
+        String message = jTextField1.getText();
+        jTextField1.setText(""); //Should reset text in JTextField
+        coloredText(chatWindow, "You: "+message+"\n", Color.GREEN);
+    } // end of bSend_ActionPerformed
+
+
+    //Make text Colorful
+    public void coloredText(JTextPane pane, String message, Color color){
+        StyledDocument doc = pane.getStyledDocument();
+        Style style = pane.addStyle("Color Style", null);
+        StyleConstants.setForeground(style, color);
+        try{
+            doc.insertString(doc.getLength(), message, style);
+        }
+        catch(BadLocationException e){
+            e.printStackTrace();
+        }
+    }
+
 }
