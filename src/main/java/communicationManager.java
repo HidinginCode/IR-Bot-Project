15f79bf8587a8d924a44fa7b4b839859c4295781
HidinginCode <86361594+HidinginCode@users.noreply.github.com>
2022-01-13 18:48:29 +0100

    First Beta version

--- src/main/java/communicationManager.java
+++ src/main/java/communicationManager.java
@@ -1,4 +1,5 @@
-import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
@@ -15,7 +16,6 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class communicationManager {
     dataManager dm = new dataManager();
@@ -27,12 +27,14 @@ public class communicationManager {
     public String getMessageText(String message) throws IOException, ParseException {
         message = message.toLowerCase();
         List<String> messageList = new ArrayList<>();
+        CharArraySet stopSet = StopFilter.makeStopSet("what", "is", "can", "you", "tell", "me");
         StandardTokenizer tokenizer = new StandardTokenizer();
         tokenizer.setReader(new StringReader(message));
-        final CharTermAttribute termAttr = tokenizer.addAttribute(CharTermAttribute.class);
-        tokenizer.reset();
+        StopFilter stopTokenizer = new StopFilter(tokenizer, stopSet);
+        final CharTermAttribute termAttr = stopTokenizer.addAttribute(CharTermAttribute.class);
+        stopTokenizer.reset();
 
-        while(tokenizer.incrementToken()){
+        while(stopTokenizer.incrementToken()){
             messageList.add(termAttr.toString());
         }
         System.out.println(messageList.toString());
@@ -44,28 +46,57 @@ public class communicationManager {
             String returnString = "Currently I have stored "+dm.getNumberOfDocs()+" documents in my database.";
             return returnString;
         }
-        else{
+        else {
+            //Make a query for every word that has not been filtered
             List<Query> queryList = new ArrayList<>(); //List of querys
-            for (int i = 0; i < messageList.size(); i++){
+            for (int i = 0; i < messageList.size(); i++) {
                 Query q = new QueryParser("content", new StandardAnalyzer()).parse(messageList.get(i)); //Make a list of querys for each word
                 queryList.add(q);
             }
+
+            //Get the Top Docs for every query and save it to list
             List<TopDocs> topDocs = new ArrayList(); //List of TopDocs
-            IndexReader reader= DirectoryReader.open(dataManager.dir);
+            IndexReader reader = DirectoryReader.open(dataManager.dir);
             IndexSearcher searcher = new IndexSearcher(reader);
-            for(int i = 0; i < queryList.size(); i++){
+            for (int i = 0; i < queryList.size(); i++) {
                 TopDocs docs = searcher.search(queryList.get(i), 10);
                 topDocs.add(docs);
             }
 
+            //Convert topdocs to arrays and save arrays to list
             List<ScoreDoc[]> hitsList = new ArrayList<>();
-            for(int i = 0; i < queryList.size(); i++){
+            for (int i = 0; i < queryList.size(); i++) {
                 hitsList.add(topDocs.get(i).scoreDocs);
             }
-            System.out.println(hitsList.get(0)[0].doc);
-            System.out.println(reader.document(hitsList.get(0)[0].doc).getField("content").toString());
+            if (hitsList.size() != 0) {
+
+                float highestScore = 0;
+                int docId = 0;
+
+                for (int i = 0; i < hitsList.size(); i++) {
+                    for (int j = 0; j < hitsList.get(i).length; j++) {
+                        if (hitsList.get(i)[j].score > highestScore) {
+                            highestScore = hitsList.get(i)[j].score;
+                            docId = hitsList.get(i)[j].doc;
+                        }
+                    }
+                }
+                String answer = reader.document(docId).getField("content").stringValue();
+                answer = answerPreprocessing(answer);
+                return answer;
+            }
+            else{
+                return "Sorry I didn't understand :(";
+            }
         }
-        return "Sorry I didn't understand :(";
+    }
+
+    public String answerPreprocessing(String answer){
+        if(answer.startsWith(".") || answer.startsWith("!") || answer.startsWith(" ") || answer.startsWith(",")){
+            answer = answer.substring(1);
+            return answerPreprocessing(answer);
+        }
+        return answer;
     }
 
 }
